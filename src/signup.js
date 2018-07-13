9d0fc1c7aca91318c17bd505f6eae6492f8657a7
mkucharz <m@kucharz.net>
2018-07-13 08:53:18 +0200

    fix(): style

--- src/signup.js
+++ src/signup.js
@@ -1,33 +1,25 @@
 import Syncano from '@syncano/core'
+import Validator from '@syncano/validate'
 import axios from 'axios'
 import { getMailchimpUrl } from './utils'
 
 export default async ctx => {
   const {response, logger} = new Syncano(ctx)
+  const validator = new Validator(ctx)
   const {info, warn} = logger('user-profile:get')
   const {MAILCHIMP_API_KEY} = ctx.config
   const {listId, email} = ctx.args
 
   try {
-    if (!listId) {
-      throw new Error('Invalid listId parameter.')
-    }
-    if (!email) {
-      throw new Error('Email is required.')
-    }
-    if (!isEmail(email)) {
-      throw new Error('Invalid email.')
-    }
-  } catch ({message}) {
-    warn(message)
-    return response.json({message}, 400)
+    await validator.validateRequest()
+  } catch (err) {
+    return response.json(err.messages, 400)
   }
 
   const endpoint = getMailchimpUrl(MAILCHIMP_API_KEY, listId)
 
   try {
-    await axios(endpoint, {
-      method: 'post',
+    await axios.post(endpoint, {
       data: {
         email_address: email,
         status: 'pending'
@@ -36,6 +28,7 @@ export default async ctx => {
         authorization: `apikey ${MAILCHIMP_API_KEY}`
       }
     })
+
     info(`Subscribed ${email} to list ${listId}`)
     response.json({
       message: `You're now subscribed. Thanks!`
@@ -48,11 +41,4 @@ export default async ctx => {
     warn(message)
     response.json({message}, 400)
   }
-}
-
-export function isEmail (str) {
-  // eslint-disable-next-line
-  const regex = /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$/i
-
-  return regex.test(str)
 }
